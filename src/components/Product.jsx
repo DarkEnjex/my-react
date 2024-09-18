3dcc0f569aa04368be96d83b9b1c1d79743fea28
Valeri <mylenovo21072017@gmail.com>
2024-09-18 20:37:59 +0300

    add some from modul 1

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,44 +1,13 @@
-// export default function Product() {
-//   return (
-//     <div>
-//       <h2>Tacos</h2>
-//       <p>Price: 999 credits</p>
-//     </div>
-//   );
-// }
-
-//? 2 Приклад
-// src/components/Product.jsx
-
-// export default function Product() {
-//   return (
-//     <>
-//       <h2>Tacos</h2>
-//       <img
-//         src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-//         alt="Tacos With Lime"
-//         width="640"
-//       />
-//       <p>Price: 999 credits</p>
-//     </>
-//   );
-// }
-
-//? 3 Приклад
-// src/components/Product.jsx
-
-// src/components/Product.jsx
-
 export default function Product({
-  name,
+  name = "Not found, try again",
   imgUrl = "https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder",
-  price,
+  price = "???",
 }) {
   return (
-    <div>
-            <h2>{name}</h2>     {" "}
-      <img src={imgUrl} alt={name} width="480" /> 
-          <p>Price: {price} credits</p>   {" "}
-    </div>
+    <>
+      <h2>{name}</h2>
+      <img src={imgUrl} alt={name} width="480" />
+      <p>Price: {price} credits</p>
+    </>
   );
 }
