9ebd2b2cab803b1238ef1c2f72c6fd7969978f0a
Valeri <mylenovo21072017@gmail.com>
2024-09-23 17:21:35 +0300

    add some modul 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import "modern-normalize";
@@ -7,8 +6,4 @@ import "./index.css";
 
 createRoot(
   document.getElementById("root")
-).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+).render(<App />);
