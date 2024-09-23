a1b19126d223b5a3f52c5c0e6d335f65f00fc33e
Valeri <mylenovo21072017@gmail.com>
2024-09-23 17:48:07 +0300

    add btn show/hide

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -85,19 +85,47 @@
 // }
 
 // ?
+// import { useState } from "react";
+
+// export default function App() {
+//   const [clicks, setClicks] = useState(0);
+
+//   const handleClick = () => {
+//     // clicks = clicks + 1;
+//     setClicks(clicks + 1);
+//   };
+
+//   return (
+//     <button onClick={handleClick}>
+//       Current: {clicks}
+//     </button>
+//   );
+// }
+
+// ?
 import { useState } from "react";
 
 export default function App() {
   const [clicks, setClicks] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
-    // clicks = clicks + 1;
     setClicks(clicks + 1);
   };
 
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <button onClick={handleClick}>
-      Current: {clicks}
-    </button>
+    <>
+      <button onClick={handleClick}>
+        Current: {clicks}
+      </button>
+      <button onClick={handleToggle}>
+        {isOpen ? "Hide" : "Show"}
+      </button>
+      {isOpen && <p>Now u can see me!</p>}
+    </>
   );
 }
