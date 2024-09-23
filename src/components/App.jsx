2fab88758b3b82a0b47ff17ab7cb6a6d94e98046
Valeri <mylenovo21072017@gmail.com>
2024-09-23 17:53:47 +0300

    add 2 click

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -103,29 +103,41 @@
 // }
 
 // ?
-import { useState } from "react";
+// import { useState } from "react";
+
+// export default function App() {
+//   const [clicks, setClicks] = useState(0);
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const handleClick = () => {
+//     setClicks(clicks + 1);
+//   };
+
+//   const handleToggle = () => {
+//     setIsOpen(!isOpen);
+//   };
+
+//   return (
+//     <>
+//       <button onClick={handleClick}>
+//         Current: {clicks}
+//       </button>
+//       <button onClick={handleToggle}>
+//         {isOpen ? "Hide" : "Show"}
+//       </button>
+//       {isOpen && <p>Now u can see me!</p>}
+//     </>
+//   );
+// }
+
+// ?
+import ClickCounter from "./ClickCounter.jsx";
 
 export default function App() {
-  const [clicks, setClicks] = useState(0);
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleClick = () => {
-    setClicks(clicks + 1);
-  };
-
-  const handleToggle = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
     <>
-      <button onClick={handleClick}>
-        Current: {clicks}
-      </button>
-      <button onClick={handleToggle}>
-        {isOpen ? "Hide" : "Show"}
-      </button>
-      {isOpen && <p>Now u can see me!</p>}
+      <ClickCounter />
+      <ClickCounter />
     </>
   );
 }
